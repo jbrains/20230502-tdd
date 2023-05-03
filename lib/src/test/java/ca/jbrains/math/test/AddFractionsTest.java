cca16ee539ee6e420c3a57ce6ad27951ba4d1cc5
J. B. Rainsberger <me@jbrains.ca>
2023-05-03 11:57:10 -0300

    Removed duplication: collapsed a redundant if statement.

--- lib/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ lib/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -37,12 +37,7 @@ public class AddFractionsTest {
         }
 
         public Fraction plus(Fraction that) {
-            if (that.integerValue == 0)
-                return new Fraction(this.integerValue);
-            else if (this.integerValue == 0)
-                return new Fraction(that.integerValue);
-            else
-                return new Fraction(this.integerValue + that.integerValue);
+            return new Fraction(this.integerValue + that.integerValue);
         }
 
         public int intValue() {
