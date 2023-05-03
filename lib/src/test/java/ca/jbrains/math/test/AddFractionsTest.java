a7d796def55fba4b77006e1673c14f2d4500688c
J. B. Rainsberger <me@jbrains.ca>
2023-05-03 12:03:48 -0300

    Removed an obsolete import.

--- lib/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ lib/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -1,7 +1,6 @@
 package ca.jbrains.math.test;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class AddFractionsTest {
