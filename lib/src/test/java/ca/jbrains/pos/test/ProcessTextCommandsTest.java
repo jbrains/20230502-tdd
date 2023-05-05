f74e01c00c48be3b7b80d6ac6a15f452a88b0c00
J. B. Rainsberger <me@jbrains.ca>
2023-05-05 11:43:47 -0300

    We now handle 0 commands.

--- lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 public class ProcessTextCommandsTest {
@@ -26,16 +27,25 @@ public class ProcessTextCommandsTest {
         Assertions.assertEquals("output from command 1\n", canvas.toString());
     }
 
+    @Test
+    void noCommands() {
+        final StringReader simulatedTextInput = new StringReader("");
+        final StringWriter canvas = new StringWriter();
+
+        process(simulatedTextInput, canvas);
+
+        Assertions.assertEquals("", canvas.toString());
+    }
+
     private void process(StringReader textInput, StringWriter canvas) {
         // This is complicated, but it's the easiest way I know to get lines of text
         // from a Reader. I'm open to better suggestions. :)
         final Stream<String> textInputAsLines = new BufferedReader(textInput).lines();
 
-        // Our only test provides 1 line of text, so we can safely assume that there
-        // is 1 line of text. If not, then this should fail with an exception.
-        final String line = textInputAsLines.iterator().next();
-
-        // We don't even need to look at the line of text yet!
-        new PrintWriter(canvas, true).println("output from command 1");
+        final Iterator<String> textInputAsLinesIterator = textInputAsLines.iterator();
+        if (textInputAsLinesIterator.hasNext()) {
+            // We don't even need to look at the line of text yet!
+            new PrintWriter(canvas, true).println("output from command 1");
+        }
     }
 }
