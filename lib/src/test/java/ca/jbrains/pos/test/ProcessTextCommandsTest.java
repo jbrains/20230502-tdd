1385b2e9ad7a1d055529490847a753ec153ae8b5
J. B. Rainsberger <me@jbrains.ca>
2023-05-05 11:30:45 -0300

    We can process any text commands, as long as the first command is "quit".

--- lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
@@ -1,9 +1,28 @@
 package ca.jbrains.pos.test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
 public class ProcessTextCommandsTest {
     // Test List:
     // - happy path, process lines until "quit":
     //    - quit on the first line
     //    - 1 command, then quit
     //    - many commands, then quit
+
+    @Test
+    void quitBeforeAnyCommands() {
+        final StringReader simulatedTextInput = new StringReader("quit\n");
+        final StringWriter canvas = new StringWriter();
+
+        process(simulatedTextInput, canvas);
+
+        Assertions.assertEquals("", canvas.toString());
+    }
+
+    private void process(StringReader textInput, StringWriter canvas) {
+    }
 }
