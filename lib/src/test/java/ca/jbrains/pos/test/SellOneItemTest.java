2547f6ae3c7d13ab1030e467aab6c59206fb93e2
J. B. Rainsberger <me@jbrains.ca>
2023-05-05 10:12:33 -0300

    Collapsed a redundant if block.

--- lib/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -51,14 +51,12 @@ public class SellOneItemTest {
         public void onBarcode(String barcode) {
             if ("".equals(barcode))
                 display.setText("Scanning error: empty barcode");
-            else {
-                if ("12345".equals(barcode))
-                    display.setText("CAD 7.95");
-                else if ("23456".equals(barcode))
-                    display.setText("CAD 12.50");
-                else
-                    display.setText("Product not found: " + barcode);
-            }
+            else if ("12345".equals(barcode))
+                display.setText("CAD 7.95");
+            else if ("23456".equals(barcode))
+                display.setText("CAD 12.50");
+            else
+                display.setText("Product not found: " + barcode);
         }
     }
 
