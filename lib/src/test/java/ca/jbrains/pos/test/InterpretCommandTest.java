a1ccd7cdcfe0b576293006d0278dc426d205f99d
J. B. Rainsberger <me@jbrains.ca>
2023-05-05 12:27:02 -0300

    [FAILING] Now we focus on interpretCommand(), which starts to look a lot like Sale.onBarcode(). :)

--- lib/src/test/java/ca/jbrains/pos/test/InterpretCommandTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/InterpretCommandTest.java
@@ -1,17 +1,26 @@
 package ca.jbrains.pos.test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 // CONTRACT:
 // This behavior receives 1 line of text at a time,
 // but it receives "raw text". This means that it
 // will need to both clean up the text and then try
 // to understand it.
-//
-// The client will have the responsibility of running
-// a "UI loop", so this behavior expects never to receive
-// a "quit" command.
-public class ParseCommandTest {
+public class InterpretCommandTest {
     // Test list:
     // - happy path: interpret a valid command as "barcode scanned"
     // - extra whitespace: trim from the beginning and end
     // - empty: ignore the command (how?)
+
+
+    @Test
+    void happyPath() {
+        Assertions.assertEquals("CAD 7.95", interpretCommand("12345"));
+    }
+
+    private String interpretCommand(String textCommand) {
+        return "I don't understand you";
+    }
 }
