46e13f3aad6ce7df71a16a07271e00114ab2cb91
J. B. Rainsberger <me@jbrains.ca>
2023-05-05 11:35:39 -0300

    We can process either 0 or 1 text commands, but no more, and only if the "quit" command is correct.

--- lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
@@ -3,15 +3,18 @@ package ca.jbrains.pos.test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.stream.Stream;
 
 public class ProcessTextCommandsTest {
     // Test List:
     // - happy path, process lines until "quit":
-    //    - quit on the first line
     //    - 1 command, then quit
     //    - many commands, then quit
+    //    - premature end of stream
 
     @Test
     void quitBeforeAnyCommands() {
@@ -23,6 +26,23 @@ public class ProcessTextCommandsTest {
         Assertions.assertEquals("", canvas.toString());
     }
 
+    @Test
+    void oneCommandThenQuit() {
+        final StringReader simulatedTextInput = new StringReader("12345\nquit\n");
+        final StringWriter canvas = new StringWriter();
+
+        process(simulatedTextInput, canvas);
+
+        Assertions.assertEquals("CAD 7.95\n", canvas.toString());
+    }
+
     private void process(StringReader textInput, StringWriter canvas) {
+        final Stream<String> textInputAsLines = new BufferedReader(textInput).lines();
+        final PrintWriter out = new PrintWriter(canvas, true);
+
+        for (String line : textInputAsLines.toList()) {
+            if ("quit".equals(line)) break;
+            out.println("CAD 7.95");
+        }
     }
 }
