68a43639a5fc4458aa8e6754c1cf2b398c0da044
J. B. Rainsberger <me@jbrains.ca>
2023-05-24 14:53:47 -0300

    Extracted production code for processing text commands.

---
+++ lib/src/test/java/ca/jbrains/pos/test/CommandProcessor.java
@@ -0,0 +1,26 @@
+package ca.jbrains.pos.test;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public final class CommandProcessor {
+    private final CommandInterpreter commandInterpreter;
+
+    public CommandProcessor(CommandInterpreter commandInterpreter) {
+        this.commandInterpreter = commandInterpreter;
+    }
+
+    void process(Reader textInput, Writer canvas) {
+        final PrintWriter out = new PrintWriter(canvas, true);
+
+        final Stream<String> textInputAsLines = new BufferedReader(textInput).lines();
+
+        final Stream<String> outputLines = textInputAsLines.map(commandInterpreter::interpretCommand);
+
+        outputLines.forEachOrdered(outputLine -> out.println(outputLine));
+    }
+}
--- lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
+++ lib/src/test/java/ca/jbrains/pos/test/ProcessTextCommandsTest.java
@@ -7,7 +7,6 @@ import java.io.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class ProcessTextCommandsTest {
     // These tests no longer worry about _how_ to interpret commands,
@@ -20,10 +19,10 @@ public class ProcessTextCommandsTest {
         final StringReader simulatedTextInput = new StringReader("command 1\n");
         final StringWriter canvas = new StringWriter();
 
-        process(simulatedTextInput, canvas, new LightweightCommandInterpreter(
+        new CommandProcessor(new LightweightCommandInterpreter(
                 Map.of("command 1", "output from command 1",
                         "command 2", "output from command 2",
-                        "command 3", "output from command 3")));
+                        "command 3", "output from command 3"))).process(simulatedTextInput, canvas);
 
         Assertions.assertEquals(List.of("output from command 1"), lines(canvas.toString()));
     }
@@ -33,10 +32,10 @@ public class ProcessTextCommandsTest {
         final StringReader simulatedTextInput = new StringReader("");
         final StringWriter canvas = new StringWriter();
 
-        process(simulatedTextInput, canvas, new LightweightCommandInterpreter(
+        new CommandProcessor(new LightweightCommandInterpreter(
                 Map.of("command 1", "output from command 1",
                         "command 2", "output from command 2",
-                        "command 3", "output from command 3")));
+                        "command 3", "output from command 3"))).process(simulatedTextInput, canvas);
 
         Assertions.assertEquals(Collections.emptyList(), lines(canvas.toString()));
     }
@@ -46,10 +45,10 @@ public class ProcessTextCommandsTest {
         final StringReader simulatedTextInput = new StringReader("command 1\ncommand 2\ncommand 3\n");
         final StringWriter canvas = new StringWriter();
 
-        process(simulatedTextInput, canvas, new LightweightCommandInterpreter(
+        new CommandProcessor(new LightweightCommandInterpreter(
                 Map.of("command 1", "output from command 1",
                         "command 2", "output from command 2",
-                        "command 3", "output from command 3")));
+                        "command 3", "output from command 3"))).process(simulatedTextInput, canvas);
 
         Assertions.assertEquals(
                 List.of(
@@ -63,26 +62,16 @@ public class ProcessTextCommandsTest {
         return new BufferedReader(new StringReader(multilineText)).lines().toList();
     }
 
-    private void process(Reader textInput, Writer canvas, CommandInterpreter commandInterpreter) {
-        final PrintWriter out = new PrintWriter(canvas, true);
-
-        final Stream<String> textInputAsLines = new BufferedReader(textInput).lines();
-
-        final Stream<String> outputLines = textInputAsLines.map(commandInterpreter::interpretCommand);
-
-        outputLines.forEachOrdered(outputLine -> out.println(outputLine));
-    }
-
     private static final class LightweightCommandInterpreter implements CommandInterpreter {
-        private final Map<String, String> commandInterpreter;
+        private final Map<String, String> script;
 
-        private LightweightCommandInterpreter(Map<String, String> commandInterpreter) {
-            this.commandInterpreter = commandInterpreter;
+        private LightweightCommandInterpreter(Map<String, String> script) {
+            this.script = script;
         }
 
         @Override
         public String interpretCommand(String line) {
-            return commandInterpreter.get(line);
+            return script.get(line);
         }
     }
 }
