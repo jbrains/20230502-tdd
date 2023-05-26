97a0118878194de655cb64a43dec57ab6e8a750f
J. B. Rainsberger <me@jbrains.ca>
2023-05-26 09:39:21 -0300

    We now have a repeatable Golden Master test with a sample of 1.

--- src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -1,13 +1,14 @@
 package com.adaptionsoft.games.trivia;
 
 import com.adaptionsoft.games.trivia.runner.GameRunner;
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
+import java.util.Random;
 
 public class GoldenMasterTest {
     @Test
@@ -16,10 +17,37 @@ public class GoldenMasterTest {
 
         System.setOut(new PrintStream(fakeStdoutStream));
 
-        GameRunner.main(new String[0]);
+        runGame();
 
         final String allTextFromStdout = fakeStdoutStream.toString(Charset.forName("UTF-8"));
 
         Approvals.verify(allTextFromStdout);
+    }
+
+    private static void runGame() {
+        boolean notAWinner;
+
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        Random rand = new Random(762);
+
+        do {
+
+            aGame.roll(rand.nextInt(5) + 1);
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+
+
+        } while (notAWinner);
+
     }
 }
